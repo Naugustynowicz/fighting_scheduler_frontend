c6a5d2ca27b674297babfec7f7d37e43e63f4465
naugustynowicz <nicolas.augustynowicz@gmail.com>
2025-04-01 11:23:32 +0200

    dockerise project

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,10 +4,21 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: 'tests/setupTest.js',
   },
+  preview: {
+    port: 5173,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true, // NB : address the server listen to, here listen to everything
+    origin: "http://0.0.0.0:5173", 
+   },
 })
